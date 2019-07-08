c6dbf18f0b9762cc102601760d977c6a693526b0
Ariel Soares <arielsoares89@gmail.com>
2019-07-08 17:24:41 -0300

    leitura do json.

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,23 +1,20 @@
 import axios from 'axios';
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo One',
-        },
-        {
-            id: 2,
-            title: 'Todo Two'
-        }
-    ]
+    todos: []
 };
 
 const getters = {
-    allTodos: (state) => state.todos
+    allTodos: state => state.todos
 };
+//commit serve para não criar a mutation diretamente
+const actions = {
+    async fetchTodos({ commit }) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
-const actions = {};
+        console.log(response.data);
+    }
+};
 
 const mutations = {};
 
